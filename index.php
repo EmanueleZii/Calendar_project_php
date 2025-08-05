c7ce2da65fece363d4a08146360cb4c5e4b28221
Emanuele Zignale <zignale92@gmail.com>
2025-08-05 10:48:19 +0200

    index complete and calendar.js created

--- index.php
+++ index.php
@@ -42,37 +42,55 @@
     </div>
 
     <!--Modal For Add / Edit / Delete Appointment-->
-    <div class="eventSelectorWrapper">
-        <label for="eventSelector">
-            <strong>
-                Seleziona Evento:
-            </strong>
-        </label>
-        <select name="" id="eventSelector" onchange="">
-            <option value="" disabled selected>Scegli L'evento</option>
-        </select>
+    <div class="modal" id="eventModal">
+        <div class="modal-content">
+            <div class="eventSelectorWrapper">
+                <label for="eventSelector">
+                    <strong>
+                        Seleziona Evento:
+                    </strong>
+                </label>
+                <select name="" id="eventSelector" onchange="">
+                    <option value="" disabled selected>Scegli L'evento</option>
+                </select>
+            </div>
+
+            <!-- Main Form-->
+            <form method="POST" action="" id="eventForm">
+                <input type="hidden" name="action"  value="add" id="formAction">
+                <input type="hidden" name="event_id" id="eventId">
+
+                <label for="courseName">Titolo Del Corso</label>
+                <input type="text" name="course_name" id="courseName" required>
+
+                <label for="instructorName">Nome dell' Istruttore</label>
+                <input type="text" name="instyructor_name" id="instructorName" required>
+
+                <label for="startDate">Data Di Inizio:</label>
+                <input type="date" name="start_date" id="start_date" required />
+
+                <label for="endDate">Data Di Fine:</label>
+                <input type="date" name="end_date" id="endDate" required>
+
+                <button type="submit">💾 Salva</button>
+                
+            </form>
+
+            <!--Delete Form-->
+            <form action="" method="POST" onsubmit="return confirm('Sei Sicuro di cancellare?')">
+                <input type="hidden" name="action" value="delete">
+                <input type="hidden" name="event_id" id="deleteEventId">
+                <button type="submit" class="dubmit-btn">🗑️ Elimina </button>
+            </form>
+
+            <!--❌ Cancel -->
+            <button type="button" onclick="" class="submit-btn">❌ Cancella </button>
+        </div>
     </div>
+    
+    <script src="calendar.js">
 
-    <!-- Main Form-->
-    <form method="POST" action="" id="eventForm">
-        <input type="hidden" name="action"  value="add" id="formAction">
-        <input type="hidden" name="event_id" id="eventId">
-
-        <label for="courseName">Titolo Del Corso</label>
-        <input type="text" name="course_name" id="courseName" required>
-
-        <label for="instructorName">Nome dell' Istruttore</label>
-        <input type="text" name="instyructor_name" id="instructorName" required>
-
-        <label for="startDate">Data Di Inizio:</label>
-        <input type="date" name="start_date" id="start_date" required />
-
-        <label for="endDate">Data Di Fine:</label>
-        <input type="date" name="end_date" id="endDate" required>
-
-        <button type="submit">💾 Salva</button>
-        
-    </form>
+    </script>
 
 </body>
 </html>
