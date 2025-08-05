c627012fa61575d040f66c5f37895d88e142ea5a
Emanuele Zignale <zignale92@gmail.com>
2025-08-05 17:52:02 +0200

    finish php script

--- calendar.php
+++ calendar.php
@@ -89,7 +89,46 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && ($_POST['action'] ?? '') === 'delet
 }
 
 ##Success And Error Messagge
-if (isset()){
-    
+if (isset($_GET['success']))
+{
+    $successMsg = match($_GET["success"]){
+        '1'=> "✅ Appuntamento segnato con successo",
+        '2' =>'✅ Appuntamento aggiornato con successo',
+        '3'=>'✅ Appuntamento eliminato con successo',
+        default =>""
+    };
 }
+
+if (isset($_GET['error']))
+{
+    $errorMsg = match($_GET["error"]){
+        '1'=> "❌ Errore non è stato possibile segnare l'Appuntamento ",
+        '2' =>'❌ Errore non è stato possibile aggiornare Appuntamento ',
+        '3'=>'❌ Errore non è stato possibile Eliminare Appuntamento ',
+        default=>""
+    };
+}
+
+$result = $conn->query("SELECT * FROM appointments");
+
+if($result && $result->num_rows> 0){
+    while($row = $result->fetch_assoc()){
+        $start = new DateTime($row['start_date']);
+        $end = new DateTime($row['end_date']);
+
+        while($start <= $end ){
+            $eventsFromDB [] =[
+                'id' => $row["id"],
+                'title' => "{$row['course_name']} - {$row['instructor_name']}",
+                "date" => $start->format("D-m-y"),
+                "start"=> $row['start_date'],
+                "end"=> $row['end_date']
+            ];
+            $start->modify("+1 day");
+        }
+    }
+}
+
+$conn->close();
+
 ?>
